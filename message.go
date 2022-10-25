56c429c41ecb81fcfa9b08d1bac036542d42d6a9
calvin <hhaou@outlook.com>
2022-10-26 00:17:06 +0800

    add message pool

--- message.go
+++ message.go
@@ -1,6 +1,32 @@
 package network
 
+import "sync"
+
 type Message struct {
 	ID   uint64
 	Data []byte
+}
+
+var msgPool = sync.Pool{
+	New: func() interface{} {
+		return &Message{}
+	},
+}
+
+// GetPooledMessage gets a pooled Message.
+func GetPooledMessage() *Message {
+	return msgPool.Get().(*Message)
+}
+
+// FreeMessage puts a Message into the pool.
+func FreeMessage(msg *Message) {
+	if msg != nil && len(msg.Data) > 0 {
+		ResetMessage(msg)
+		msgPool.Put(msg)
+	}
+}
+
+//ResetMessage reset a Message
+func ResetMessage(m *Message) {
+	m.Data = m.Data[:0]
 }
